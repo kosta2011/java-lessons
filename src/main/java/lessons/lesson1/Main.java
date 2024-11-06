4215f95d3127dc7c686407b30cc66d8415286fbe
kosta <kosta4job@yandex.ru>
2024-11-06 22:09:02 +0300

    first test commit

---
+++ src/main/java/lessons/lesson1/Main.java
@@ -0,0 +1,12 @@
+package lessons.lesson1;
+
+public class Main {
+
+    public static int someMethod(int i) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
---
+++ src/main/java/lessons/lesson2/Main.java
@@ -0,0 +1,7 @@
+package lessons.lesson2;
+
+public class Main {
+    public static int someMethod(int i) {
+        return 0;
+    }
+}
