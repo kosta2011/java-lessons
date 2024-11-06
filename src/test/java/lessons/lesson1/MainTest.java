bcd025e62fc1ede1b07866b150887c3f306c12c6
kosta <kosta4job@yandex.ru>
2024-11-06 22:09:02 +0300

    first test commit

---
+++ src/test/java/lessons/lesson1/MainTest.java
@@ -0,0 +1,11 @@
+package lessons.lesson1;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testSolution() {
+        assertEquals(4, Main.someMethod(2));
+    }
+}
---
+++ src/test/java/lessons/lesson2/MainTest.java
@@ -0,0 +1,11 @@
+package lessons.lesson2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void testSolution() {
+        assertEquals(4, Main.someMethod(2));
+    }
+}
